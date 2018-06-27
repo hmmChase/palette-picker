5882e30225a14fbbafb370666ffe905033e705b2
hmmChase <hmmchase@gmail.com>
2018-06-26 21:01:35 -0600

    Add ability to lock colors

--- public/scripts.js
+++ public/scripts.js
@@ -7,14 +7,29 @@ const generatePalette = () => {
   $('.color-palette')
     .children()
     .each((index, element) => {
-      $('#' + element.id)
-        .css('background', randomHexColor)
-        .find('#hex-color')
-        .text(randomHexColor);
+      const $element = $('#' + element.id);
+      const haveLockClass = $element.find('.lock-icon').hasClass('locked');
+
+      if (!haveLockClass) {
+        $element
+          .css('background', randomHexColor)
+          .find('.hex-color')
+          .text(randomHexColor);
+      }
     });
 };
 
-$('#button-generate-palette').click(generatePalette);
+const toggleLock = event => {
+  const lockIcon = event.target;
+
+  lockIcon.classList.toggle('locked');
+  lockIcon.classList.contains('locked')
+    ? (lockIcon.src = './assets/lock.svg')
+    : (lockIcon.src = './assets/unlock.svg');
+};
+
+$('.lock-icon').click(() => toggleLock(event));
+$('.generate-button').click(generatePalette);
 
 $(document).ready(function() {
   generatePalette();
