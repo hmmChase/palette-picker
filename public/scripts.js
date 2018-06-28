af19f19e7c3f055cdb10d514e78e9e2843029724
hmmChase <hmmchase@gmail.com>
2018-06-27 18:04:03 -0600

    Make title letters random colors, add convertToHex function

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,50 @@
-const randomHexColor = () =>
-  '#000000'
+const randomHexColor = () => {
+  return '#000000'
     .replace(/0/g, () => (~~(Math.random() * 16)).toString(16))
     .toUpperCase();
+};
 
 const generatePalette = () => {
   $('.color-palette')
     .children()
-    .each((index, element) => {
-      const $element = $('#' + element.id);
-      const haveLockClass = $element.find('.lock-icon').hasClass('locked');
+    .each((index, colorCard) => {
+      const $colorCard = $('#' + colorCard.id);
+      const haveLockClass = $colorCard.find('.lock-icon').hasClass('locked');
+      const color = randomHexColor();
 
       if (!haveLockClass) {
-        $element
-          .css('background', randomHexColor)
+        $colorCard
+          .css('background', color)
           .find('.hex-color')
-          .text(randomHexColor);
+          .text(color);
       }
+    });
+};
+
+const getPaletteColors = () => {
+  const color1 = $('#color-sample1').css('backgroundColor');
+  const color2 = $('#color-sample2').css('backgroundColor');
+  const color3 = $('#color-sample3').css('backgroundColor');
+  const color4 = $('#color-sample4').css('backgroundColor');
+  const color5 = $('#color-sample5').css('backgroundColor');
+  const colorPalette = [color1, color2, color3, color4, color5];
+  return colorPalette.map(rbg => convertToHex(rbg));
+};
+
+const convertToHex = rgb => {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  function hex(x) {
+    return ('0' + parseInt(x).toString(16)).slice(-2);
+  }
+  return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+};
+
+const colorizeTitle = () => {
+  $('.title')
+    .children()
+    .each((index, letter) => {
+      const $letter = $('#' + letter.id);
+      $letter.css('color', randomHexColor);
     });
 };
 
@@ -33,4 +62,5 @@ $('.generate-button').click(generatePalette);
 
 $(document).ready(function() {
   generatePalette();
+  colorizeTitle();
 });
