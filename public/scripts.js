693ad7947d44b4faf6f11081a7f1987bf9401ecc
hmmChase <hmmchase@gmail.com>
2018-06-28 19:09:49 -0600

    Refactor displayPalettes Add post request to send palettes to DB

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,5 @@
 const randomHexColor = () => {
-  return '#000000'
-    .replace(/0/g, () => (~~(Math.random() * 16)).toString(16))
-    .toUpperCase();
+  return '#000000'.replace(/0/g, () => (~~(Math.random() * 16)).toString(16));
 };
 
 const generatePalette = () => {
@@ -16,28 +14,28 @@ const generatePalette = () => {
         $colorCard
           .css('background', color)
           .find('.hex-color')
-          .text(color);
+          .text(color.toUpperCase());
       }
     });
 };
 
-// const getPaletteColors = () => {
-//   const color1 = $('#color-sample1').css('backgroundColor');
-//   const color2 = $('#color-sample2').css('backgroundColor');
-//   const color3 = $('#color-sample3').css('backgroundColor');
-//   const color4 = $('#color-sample4').css('backgroundColor');
-//   const color5 = $('#color-sample5').css('backgroundColor');
-//   const colorPalette = [color1, color2, color3, color4, color5];
-//   return colorPalette.map(rbg => convertToHex(rbg));
-// };
+const getPaletteColors = () => {
+  const color1 = $('#color-sample1').css('backgroundColor');
+  const color2 = $('#color-sample2').css('backgroundColor');
+  const color3 = $('#color-sample3').css('backgroundColor');
+  const color4 = $('#color-sample4').css('backgroundColor');
+  const color5 = $('#color-sample5').css('backgroundColor');
+  const colorPalette = [color1, color2, color3, color4, color5];
+  return colorPalette.map(rbg => convertToHex(rbg));
+};
 
-// const convertToHex = rgb => {
-//   rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-//   function hex(x) {
-//     return ('0' + parseInt(x).toString(16)).slice(-2);
-//   }
-//   return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-// };
+const convertToHex = rgb => {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  function hex(x) {
+    return ('0' + parseInt(x).toString(16)).slice(-2);
+  }
+  return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+};
 
 const colorizeTitle = () => {
   $('.title')
@@ -98,39 +96,40 @@ displayProjects = projects => {
 
 displayPalettes = palettes => {
   palettes.forEach(palette => {
-    $(`.${palette.project_id}`).append(`
-    <div class="saved-palette">
-      <h4 class="palette-name">${palette.palette_name}</h4>
-      <div class="color-thumbnail" style='background-color:${
-        palette.color1
-      }'></div>
-      <div class="color-thumbnail" style='background-color:${
-        palette.color2
-      }'></div>
-      <div class="color-thumbnail" style='background-color:${
-        palette.color3
-      }'></div>
-      <div class="color-thumbnail" style='background-color:${
-        palette.color4
-      }'></div>
-      <div class="color-thumbnail" style='background-color:${
-        palette.color5
-      }'></div>
-      <img class="delete-palette-icon" src="./images/trashcan.svg" alt="trashcan">
-    </div>
-    `);
+    const colors = [
+      palette.color1,
+      palette.color2,
+      palette.color3,
+      palette.color4,
+      palette.color5
+    ];
+    appendPalette(palette.palette_name, colors, palette.project_id);
   });
 };
 
 const appendProject = (name, id) => {
   $('.select-projects').append(`
-      <option value=${name}>
+      <option value=${id}>
         ${name}</option>`);
 
   $('.saved-projects').append(`
-      <div class='project ${id}'>
+      <div class='project' id=${id}>
         <h3 class='project-name'>${name}</h3>    
       </div>`);
+};
+
+const appendPalette = (paletteName, colors, projectId) => {
+  $(`#${projectId}`).append(`
+  <div class="saved-palette">
+    <h4 class="palette-name">${paletteName}</h4>
+    <div class="color-thumbnail" style='background-color:${colors[0]}'></div>
+    <div class="color-thumbnail" style='background-color:${colors[1]}'></div>
+    <div class="color-thumbnail" style='background-color:${colors[2]}'></div>
+    <div class="color-thumbnail" style='background-color:${colors[3]}'></div>
+    <div class="color-thumbnail" style='background-color:${colors[4]}'></div>
+    <img class="delete-palette-icon" src="./images/trashcan.svg" alt="trashcan">
+  </div>
+  `);
 };
 
 saveProject = async event => {
@@ -142,8 +141,6 @@ saveProject = async event => {
   });
 
   if (!projectExists && $projectName.length) {
-    console.log('%c posted: ', 'background: #000; color: #bada55', 'posted');
-
     try {
       const options = {
         method: 'POST',
@@ -164,9 +161,49 @@ saveProject = async event => {
   }
 };
 
+const savePalette = async event => {
+  event.preventDefault();
+  const $paletteName = $('.palette-name-input').val();
+  const $projectID = $('.select-projects option:selected').val();
+  const colors = getPaletteColors();
+  const savedPalettes = await fetchPalettes();
+
+  const paletteExists = savedPalettes.some(palette => {
+    return palette.palette_name === $paletteName;
+  });
+
+  if (!paletteExists && $paletteName.length) {
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          palette_name: $paletteName,
+          color1: colors[0],
+          color2: colors[1],
+          color3: colors[2],
+          color4: colors[3],
+          color5: colors[4],
+          project_id: $projectID
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      };
+      const response = await fetch('/api/v1/palettes', options);
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      appendPalette($paletteName, colors, $projectID);
+      $('.palette-name-input').val('');
+      return await response.json();
+    } catch (error) {
+      throw new Error(`Network request failed. (error: ${error.message})`);
+    }
+  }
+};
+
 $('.lock-icon').click(() => toggleLock(event));
 $('.generate-button').click(generatePalette);
 $('.save-project').click(saveProject);
+$('.save-palette').click(savePalette);
 
 $(document).ready(function() {
   loadProjects();
